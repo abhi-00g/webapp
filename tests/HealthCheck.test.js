bf5316fcb45512642b4c8e6580ba8e4f9d10fb5e
abhi-00g <gade.venk@northeastern.edu>
2025-02-04 13:26:10 -0500

    Added tests

---
+++ tests/HealthCheck.test.js
@@ -0,0 +1,47 @@
+require('dotenv').config();
+const request = require('supertest');
+const app = require('../server');
+const sequelize = require('../config/database');
+
+jest.useFakeTimers();
+
+beforeAll(async () => {
+    await sequelize.sync();
+});
+
+afterAll(async () => {
+    await sequelize.close();
+});
+
+describe('HealthCheck API', () => {
+    it('should return 200 OK for GET /healthz', async () => {
+        const res = await request(app).get('/healthz');
+        expect(res.status).toBe(200);
+        expect(res.headers['cache-control']).toContain('no-cache');
+        expect(res.headers['pragma']).toBe('no-cache');
+    });
+
+    it('should return 405 for unsupported methods', async () => {
+        const methods = ['put', 'post', 'delete', 'patch'];
+        for (const method of methods) {
+            const res = await request(app)[method]('/healthz');
+            expect(res.status).toBe(405);
+            expect(res.headers).toHaveProperty('cache-control');
+            expect(res.headers['cache-control']).toContain('no-cache');
+            expect(res.headers).toHaveProperty('pragma');
+            expect(res.headers['pragma']).toBe('no-cache');
+        }
+    });
+
+    it('should return 400 Bad Request if request has a body', async () => {
+        const res = await request(app).get('/healthz').send({});
+        expect(res.status).toBe(400);
+    });
+
+    it('should return 503 if database is down', async () => {
+        await sequelize.close();
+        const res = await request(app).get('/healthz');
+        expect(res.status).toBe(503);
+
+    });
+});
